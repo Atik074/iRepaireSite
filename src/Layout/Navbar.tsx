c7a8007652c36c3e97b97c5deca93420ffdbade9
Atik074 <atiksmats@gmail.com>
2024-02-12 22:51:40 +0600

    animation added in hero section

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -2,21 +2,19 @@ import { Button } from "@/components/ui/button";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div className="h-16">
-            <nav className="w-full h-full max-w-[1220px] mx-auto px-[20px] bg-slate-400  flex justify-between items-center">
-                <span className="text-3xl">iRepaire</span>
-                <ul className="space-x-6">
-                    <NavLink to="">Home</NavLink>
-                    <NavLink to="">Service</NavLink>
-                    <NavLink to="/about">About</NavLink>
-                    <Button>
-                        <NavLink to="/login">Login</NavLink>
-                    </Button>
-                </ul>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="h-16">
+      <nav className="w-full h-full max-w-[1220px] mx-auto px-[20px] bg-slate-400  flex justify-between items-center">
+        <span className="text-3xl">iRepaire</span>
+        <ul className="lg:space-x-6 space-x-3">
+          <NavLink to="">Home</NavLink>
+          <NavLink to="">Service</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <Button className="inline-block">Login</Button> 
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/Pages/Home/HeroSection.tsx
+++ src/Pages/Home/HeroSection.tsx
@@ -1,25 +1,61 @@
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import mackbook from "../../assets/images/macbook-exposed.png";
+import { motion } from "framer-motion";
+
+const intro = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 5,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.3,
+      delayChildren: 1,
+    },
+  },
+};
+const introChildren = {
+  hidden: { opacity: 0, y: -200 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+};
 
 const HeroSection = () => {
   return (
-    <Container className="grid place-content-center  grid-cols-2">
-      <div>
-        <h1 className="text-[93px] font-bold">
-          <span className=" text-gray">Don't Worry.</span>
+    <Container className=" h-[calc(100vh - 64px)] mt-52   place-content-center grid grid-cols-1 lg:grid-cols-2">
+      <motion.div variants={intro} initial="hidden" animate="visible">
+        <motion.h1
+          variants={introChildren}
+          className="text-5xl lg:text-[93px] font-bold text-nowrap"
+        >
+          <span className="text-gray">Don't Worry.</span>
           <br />
-          <span>We'll fixed it</span>
-        </h1>
-        <p>
-          wllcome to <span>iReapair</span>, your non-stop palce for all kinds of{" "}
-          <span>mackbook repairs</span> and diagonistic
-        </p>
-        <Button>Book a service</Button>
-      </div>
-      <div>
-        <img src={mackbook} alt="" />
-      </div>
+          <span className="text-7xl">We'll fixed it.</span>
+        </motion.h1>
+        <motion.p
+          variants={introChildren}
+          className="text-dark-gray mb-5 mt-5 max-w-[50ch] text-lg "
+        >
+          wllcome to{" "}
+          <span className="text-primary-foreground font-semibold">
+            iReapair
+          </span>
+          , your non-stop palce for all kinds of{" "}
+          <span className="text-primary-foreground font-semibold">
+            mackbook repairs
+          </span>{" "}
+          and diagonistic
+        </motion.p>
+        <motion.div variants={introChildren}>
+          <Button>Book a service</Button>
+        </motion.div>
+      </motion.div>
+      <motion.div className=" mt-10 w-3/4 lg:w-full mx-auto">
+        <img
+          className="-rotate-[30deg] h-[80%] ml-8 object-contain"
+          src={mackbook}
+          alt=""
+        />
+      </motion.div>
     </Container>
   );
 };
